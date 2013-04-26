3489d0c008b304beec6e9c0f8b824cf5957b78ca
Gareth Williams <gareth@garethw.net>
2013-04-26 21:34:16 +0100

    Base project

---
+++ Code/LudumDare26.Mono.Windows/Program.cs
@@ -0,0 +1,26 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using System.Linq;
+#endregion
+
+namespace LudumDare26.Mono.Windows
+{
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        private static LudumDareGame game;
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            game = new LudumDareGame();
+            game.Run();
+        }
+    }
+}
---
+++ Code/LudumDare26/LudumDare26/Program.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace LudumDare26
+{
+#if WINDOWS || XBOX
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            using (LudumDareGame game = new LudumDareGame())
+            {
+                game.Run();
+            }
+        }
+    }
+#endif
+}
+
