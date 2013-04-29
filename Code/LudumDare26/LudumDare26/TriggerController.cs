ce0bef85b421859bb66d422da23905209f23f81a
Gareth Williams <gareth@garethw.net>
2013-04-29 22:40:19 +0100

    Story bits

--- Code/LudumDare26/LudumDare26/TriggerController.cs
+++ Code/LudumDare26/LudumDare26/TriggerController.cs
@@ -244,14 +244,16 @@ namespace LudumDare26
                     break;
 
                 case 19:
+                    PromptController.Instance.AddPrompt("story19", PromptController.PromptType.Text, "Gerde seems to recall that there are", true, 7000, 0);
+                    PromptController.Instance.AddPrompt("story20", PromptController.PromptType.Text, "two more valves to find.", true, 7000, 0);
+                    break;
+
+                case 20:
                     PromptController.Instance.AddPrompt("story17", PromptController.PromptType.Text, "Gerde allows herself a flicker of hope", true, 7000, 0);
                     PromptController.Instance.AddPrompt("story18", PromptController.PromptType.Text, "before returning to the task at hand.", true, 7000, 0);
                     break;
 
-                case 20:
-                    PromptController.Instance.AddPrompt("story19", PromptController.PromptType.Text, "Gerde seems to recall that there are", true, 7000, 0);
-                    PromptController.Instance.AddPrompt("story20", PromptController.PromptType.Text, "two more valves to find.", true, 7000, 0);
-                    break;
+                
             }
         }
 
